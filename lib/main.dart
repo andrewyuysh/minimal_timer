42b02b1e8ddde4c57ad99921c34a6b18f39fb254
Andrew Yu <andrewyuysh@gmail.com>
2018-10-12 15:11:28 -0400

    empty flatbutton in the middle

--- lib/main.dart
+++ lib/main.dart
@@ -99,40 +99,22 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text('${sec2str(timer.currentTime.inSeconds)}',
+      body: new Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          new Center(
+            child: new Text('${sec2str(timer.currentTime.inSeconds)}',
                 style: TextStyle(fontSize: 100.0)),
-          ],
-        ),
+          ),
+          new FlatButton(
+            onPressed: restartTimer,
+            child: new Text(
+              '',
+              style: TextStyle(fontSize: 100.0),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: restartTimer,
-        tooltip: 'Increment',
-        child: new Icon(Icons.delete),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
